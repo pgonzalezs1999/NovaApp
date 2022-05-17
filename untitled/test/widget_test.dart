e48c49da8d6b41f1477d039570872e41f58ffbc0
Alvaro Farreny <farrenyalvaro@gmail.com>
2022-05-17 17:51:15 +0200

    update

--- untitled/test/widget_test.dart
+++ untitled/test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:untitled/PantallaLogin.dart';
-
 import 'package:untitled/main.dart';
 
 void main() {
